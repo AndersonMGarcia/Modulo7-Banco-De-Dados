51a40d949f039451f956432e53173b4d154f445f
Anderson <99144364+AndersonMGarcia@users.noreply.github.com>
2022-04-06 09:17:46 -0300

    Introdução PHPMyAdmin

---
+++ 02-VisaoGeral-PHPMyAdmin-1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visão Geral do PHPMyAdmin</title>
+</head>
+<body>
+    <h1>Visão Geral do PHPMyAdmin</h1>   
+</body>
+</html>
